d5e52eb2f7499372b7003dffc840aa8d3e1a9b65
Singh M Yashwanth (MS/ECL2) <iyg1kor@KOR-V-002EL.kor.apac.bosch.com>
2024-06-19 11:37:58 +0530

    having issue with building rustbootloader

--- boards/hal/src/shakti/vajra.rs
+++ boards/hal/src/shakti/vajra.rs
@@ -16,8 +16,8 @@ pub struct FlashWriterEraser {
 
 impl FlashWriterEraser {
     pub fn new() -> Self {
-        let spi_inner = unsafe { SPIInner::new(SPI_OFFSET) };
-        spi.init();
+        let mut spi_inner = unsafe { SPIInner::new(SPI_OFFSET) };
+        spi_inner.init();
         FlashWriterEraser { spi: spi_inner }
     }
 }
@@ -33,54 +33,57 @@ impl FlashInterface for FlashWriterEraser {
     /// Returns:
     /// -  NONE
     fn hal_flash_write(&self, address: usize, data: *const u8, len: usize) {
-        let address = address as u32;
-        let len = len as u32;
-        let mut idx = 0u32;
-        let mut src = data as *mut u32;
-        let mut dst = address as *mut u32;
-        //Unlock the FLASH
-        while idx < len {
-            let data_ptr = (data as *const u32) as u32;
-            //checking if the len is more than 4 bytes to compute a 4 byte write on flash
-            if (len - idx > 3) {
-                // Enable FLASH Page writes
-                let b = spi.flash_read(dst);
 
-                spi.flash_write_enable();
-                // spi.flash_erase(0x00b0_0000);
-                spi.flash_status_register_read();
-
-                let z = spi.flash_write(dst, src);
-                src = ((src as u32) + 4) as *mut u32; // increment pointer by 4
-                dst = ((dst as u32) + 4) as *mut u32; // increment pointer by 4
-                idx += 4;
-            } else {
-                // else do a single byte write i.e. 1-byte write
-                let mut val = 0u32;
-                let val_bytes = ((&mut val) as *mut u32) as *mut u8;
-                let offset = (address + idx) - (((address + idx) >> 2) << 2); // offset from nearest word aligned address
-                dst = ((dst as u32) - offset) as *mut u32; // subtract offset from dst addr
-                unsafe {
-                    val = *dst; // assign current val at dst to val
-                                // store data byte at idx to `val`. `val_bytes` is a byte-pointer to val.
-                    *val_bytes.add(offset as usize) = *data.add(idx as usize);
-                }
-                let b = spi.flash_read(dst);
-
-                spi.flash_write_enable();
-                // spi.flash_erase(0x00b0_0000);
-                spi.flash_status_register_read();
-
-                let z = spi.flash_write(dst, val);
-
-                src = ((src as u32) + 1) as *mut u32; // increment pointer by 1
-                dst = ((dst as u32) + 1) as *mut u32; // increment pointer by 1
-                idx += 1;
-            }
-        }
-        //Lock the FLASH
-        self.hal_flash_lock();
     }
+    // fn hal_flash_write1(&mut self, address: usize, data: *const u8, len: usize) {
+    //     let address = address as u32;
+    //     let len = len as u32;
+    //     let mut idx = 0u32;
+    //     let mut src = data as u32;
+    //     let mut dst = address as u32;
+    //     //Unlock the FLASH
+    //     while idx < len {
+    //         let data_ptr = (data as *const u32) as u32;
+    //         //checking if the len is more than 4 bytes to compute a 4 byte write on flash
+    //         if (len - idx > 3) {
+    //             // Enable FLASH Page writes
+    //             let b = self.spi.flash_read(dst as u32);
+
+    //             self.spi.flash_write_enable();
+    //             // spi.flash_erase(0x00b0_0000);
+    //             self.spi.flash_status_register_read();
+
+    //             let z = self.spi.flash_write(dst as u32, src as u32);
+    //             src = ((src as u32) + 4) ; // increment pointer by 4
+    //             dst = ((dst as u32) + 4) ; // increment pointer by 4
+    //             idx += 4;
+    //         } else {
+    //             // else do a single byte write i.e. 1-byte write
+    //             let mut val = 0u32;
+    //             let val_bytes = ((&mut val) as *mut u32) as *mut u8;
+    //             let offset = (address + idx) - (((address + idx) >> 2) << 2); // offset from nearest word aligned address
+    //             dst = ((dst as u32) - offset) ; // subtract offset from dst addr
+    //             unsafe {
+    //                 val = self.spi.flash_read(dst as u32); // assign current val at dst to val
+    //                             // store data byte at idx to `val`. `val_bytes` is a byte-pointer to val.
+    //                 *val_bytes.add(offset as usize) = *data.add(idx as usize);
+    //             }
+    //             let b = self.spi.flash_read(dst as u32);
+
+    //             self.spi.flash_write_enable();
+    //             // spi.flash_erase(0x00b0_0000);
+    //             self.spi.flash_status_register_read();
+
+    //             let z = self.spi.flash_write(dst, val);
+
+    //             src = ((src as u32) + 1); // increment pointer by 1
+    //             dst = ((dst as u32) + 1);  // increment pointer by 1
+    //             idx += 1;
+    //         }
+    //     }
+    //     //Lock the FLASH
+    //     self.hal_flash_lock();
+    // }
 
     /// This method is used to erase data on flash
     ///
@@ -93,13 +96,15 @@ impl FlashInterface for FlashWriterEraser {
     ///
     /// Returns:
     /// -  NONE
-
     fn hal_flash_erase(&self, addr: usize, len: usize) {
-        self.spi.flash_read(0x00B0_0000);
 
-        self.spi.flash_write_enable();
-        self.spi.flash_erase(0x00b0_0000);
     }
+    // fn hal_flash_erase1(&mut self, addr: usize, len: usize) {
+    //     self.spi.flash_read(0x00B0_0000);
+
+    //     self.spi.flash_write_enable();
+    //     self.spi.flash_erase(0x00b0_0000);
+    // }
     /// This method is used to lock the flash
     ///
     /// Once the flash is locked no operation on flash can be perfomed.
